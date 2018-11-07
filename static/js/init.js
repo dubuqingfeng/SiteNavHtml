b23edb0a991be9f712da88e21466e1b3515b5554
dubuqingfeng <1135326346@qq.com>
2018-11-07 22:34:31 +0800

    init project.

---
+++ static/js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
+$(document).ready(function() {
+  $('select').material_select();
+});
