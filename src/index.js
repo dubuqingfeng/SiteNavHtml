2b6643584947674633e74daffc4b752ffc62bf6b
dubuqingfeng <1135326346@qq.com>
2018-11-24 21:15:07 +0800

    update ejs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import 'materialize-css/dist/css/materialize.min.css'
+import './css/style.css'
+import 'materialize-css/dist/js/materialize.min.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const leftLinks = require('./json/left_links.json');
+const rightLinks = require('./json/right_links.json');
+const recentlyUsed = require('./json/recently_used.json');
+console.log(leftLinks);
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Hello, Webpack'
+      title: '这是一个网址导航',
+      lang: 'zh-cn',
+      left_links: leftLinks,
+      right_links: rightLinks,
+      recently_used_links: recentlyUsed,
+      template: 'src/index.ejs',
     }),
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['docs'])
   ],
   devServer: {
-    contentBase: './dist',
+    contentBase: './docs',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"]
+      },
+    ],
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'docs')
   }
 };
