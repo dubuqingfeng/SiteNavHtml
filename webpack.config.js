7f7f892a0e749e5220e33587bd50517ef0f9ede5
dubuqingfeng <1135326346@qq.com>
2018-11-24 21:42:46 +0800

    add CopyWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const leftLinks = require('./json/left_links.json');
 const rightLinks = require('./json/right_links.json');
 const recentlyUsed = require('./json/recently_used.json');
@@ -16,7 +17,14 @@ module.exports = {
       recently_used_links: recentlyUsed,
       template: 'src/index.ejs',
     }),
-    new CleanWebpackPlugin(['docs'])
+    new CleanWebpackPlugin(['docs']),
+    new CopyWebpackPlugin([
+      {
+        from: './CNAME',
+        to: './CNAME',
+        toType: 'file'
+      },
+    ], {})
   ],
   devServer: {
     contentBase: './docs',
